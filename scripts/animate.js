3c92cf117352da3045748ff4e29982670b29e351
iRGBit <birgit.bachler@gmail.com>
2015-09-08 14:08:34 +1200

    They're everywhere

--- scripts/animate.js
+++ scripts/animate.js
@@ -1,5 +1,22 @@
+function SlidingDiv(bodyHeight){
+    this.randPosY = Math.floor((Math.random()*bodyHeight));
+    this.$div = $("<div>").addClass('box').appendTo('.outer');
+    this.$div.prepend('<img src="animals/kangaroo.gif" />')
+    this.slide()
+};
+
+SlidingDiv.prototype.slide = function(){
+    this.$div.css('top', this.randPosY);
+    var timer = Math.floor(5000+(Math.random()*10000))
+    this.$div.animate({left: '-133px'}, timer);
+};
+
 $(document).ready(function(){
-    $("button").click(function(){
-        $("div").animate({left: '250px'});
+    $(document).click(function(){
+        var bodyHeight = window.innerHeight
+|| document.documentElement.clientHeight
+|| document.body.clientHeight;
+        var div = new SlidingDiv(bodyHeight);
+        div.slide();
     });
 });
