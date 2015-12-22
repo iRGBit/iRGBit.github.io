f85da5c6beae549ab53d07ed21b18738249e12bb
iRGBit <birgit.bachler@gmail.com>
2015-12-22 17:00:54 +1300

    bla

--- stains/scripts/stains.js
+++ stains/scripts/stains.js
@@ -4,10 +4,12 @@ $.ajax({
     //This will retrieve the contents of the folder if the folder is configured as 'browsable'
     url: dir,
     success: function (data) {
-        //List all .png file names in the page
+        console.log(dir);
+        //Lsit all png file names in the page
         $(data).find("a:contains(" + fileextension + ")").each(function () {
-            var filename = this.href.replace(window.location.host, "").replace("http://", "");
-            $('#photos').append("<img src='" + dir + filename + "'>");
+            var filename = this.href.replace(window.location.host, "").replace("http:///", "");
+            console.log(filename);
+            $("body").append($("<img src=" + dir + filename + "></img>"));
         });
     }
 });
