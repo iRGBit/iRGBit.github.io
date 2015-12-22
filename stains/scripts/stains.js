3408aa699dfe1cb1d11f2466e457789967e961a3
iRGBit <birgit.bachler@gmail.com>
2015-12-22 17:39:44 +1300

    scrpts

--- stains/scripts/stains.js
+++ stains/scripts/stains.js
@@ -1,18 +1,12 @@
-$(document).ready(function() {
-var dir = "img/";
-var fileextension = ".png";
+var folder = "img/";
+
 $.ajax({
-    //This will retrieve the contents of the folder if the folder is configured as 'browsable'
-    url: dir,
+    url : folder,
     success: function (data) {
-        //Lsit all png file names in the page
-        $(data).find("a:contains(" + fileextension + ")").each(function () {
-            var filename = this.href.replace(window.location.host, "").replace("http:///", "");
-            $("#photos").append($("<img src=" + dir + filename + "></img>"));
+        $(data).find("a").attr("href", function (i, val) {
+            if( val.match(/\.jpg|\.png|\.gif/) ) {
+                $("#photos").append( "<img src='"+ folder + val +"'>" );
+            }
         });
-    },
-    error: function(XMLHttpRequest, textStatus, errorThrown) {
-                  alert("Status: " + textStatus); alert("Error: " + errorThrown);
-              }
-  });
+    }
 });
