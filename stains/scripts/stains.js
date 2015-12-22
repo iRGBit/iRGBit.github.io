57db93780f0ea334e081bb29f9b5b1685654145f
iRGBit <birgit.bachler@gmail.com>
2015-12-22 17:13:03 +1300

    scripx

--- stains/scripts/stains.js
+++ stains/scripts/stains.js
@@ -5,13 +5,14 @@ $.ajax({
     //This will retrieve the contents of the folder if the folder is configured as 'browsable'
     url: dir,
     success: function (data) {
-        console.log(dir);
         //Lsit all png file names in the page
         $(data).find("a:contains(" + fileextension + ")").each(function () {
             var filename = this.href.replace(window.location.host, "").replace("http:///", "");
-            console.log(filename);
-            $("body").append($("<img src=" + dir + filename + "></img>"));
+            $("#photos").append($("<img src=" + dir + filename + "></img>"));
         });
     }
+    error: function(XMLHttpRequest, textStatus, errorThrown) {
+                  alert("Status: " + textStatus); alert("Error: " + errorThrown);
+              } 
   });
 });
